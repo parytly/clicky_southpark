9e7f33ee023e880eee7ee3444cf6ef6576a6c9f9
Pary Ly <paryly@Parys-MacBook-Pro.local>
2020-02-23 07:33:44 -0800

    Updates

---
+++ precache-manifest.7f30f3e3da93d55e644e31b6ead96c39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10e70fe6355e13a1a6e92ed5c8c2676",
+    "url": "/clicky_southpark/index.html"
+  },
+  {
+    "revision": "11c107fe17bf58de7bcb",
+    "url": "/clicky_southpark/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "5c10912c167e746ca978",
+    "url": "/clicky_southpark/static/css/main.78412f04.chunk.css"
+  },
+  {
+    "revision": "11c107fe17bf58de7bcb",
+    "url": "/clicky_southpark/static/js/2.8a21e6fc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky_southpark/static/js/2.8a21e6fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c10912c167e746ca978",
+    "url": "/clicky_southpark/static/js/main.8ff8b975.chunk.js"
+  },
+  {
+    "revision": "aa950ed287b79d16c8e1",
+    "url": "/clicky_southpark/static/js/runtime-main.0e2ad3c4.js"
+  },
+  {
+    "revision": "66c9cc10f33fdef7c2e9a1856d344e46",
+    "url": "/clicky_southpark/static/media/southpark_background.66c9cc10.jpg"
+  }
+]);
